e0e7b8764fd5cfa6945cce6e8d02dc13a556d2cf
karlSimmaa <24254@burnside.school.nz>
2026-05-22 12:08:33 +1200

    add diagrams

--- quiz.py
+++ quiz.py
@@ -1,3 +1,19 @@
 """Customizable quiz python code for any subject, with a database to store questions and answers.
 14/05/26 by KARL"""
 import sqlite3
+
+DB = 'quiz.db'
+
+#functions
+def initialize_data():
+    conn = sqlite3.connect(DB)
+    cursor = conn.cursor()
+    cursor.execute("SELECt id, questions, answers FROM questions")
+    rows = cursor.fetchall()
+    ids = [row[0] for row in rows]  
+    question = [row[1] for row in rows]
+    answer = [row[2] for row in rows]
+    return ids, question, answer
+
+def check_answer():
+    ids, question, answer = initialize_data()
